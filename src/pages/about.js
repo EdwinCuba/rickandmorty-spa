72a66893a2ea03c92c6000838a85327377f7b300
EdwinOlaffCubaDavila <edwincubad@gmail.com>
2021-01-21 18:17:46 -0500

    Added About and fixed Logo

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+const about = () => {
+  const view = `
+    <section>
+      <h2>Que es una SPA?</h2>
+      <p>Una SPA(Single Page Application)</p>
+    </section>
+  `;
+  return view 
+}
--- src/templates/header.js
+++ src/templates/header.js
@@ -1,7 +1,7 @@
 const Header= () => {
   const view = `
     <section class="header__logo">
-      <a href="/">
+      <a href="/rickandmorty-spa">
         <img 
           src="https://assets.stickpng.com/images/58f37720a4fa116215a9240f.png" 
           alt="Logo"/>
